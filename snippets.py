a6dc866cfc9e291484f758590618f97dd0cc3452
Phinna <hhh34@nau.edu>
2015-04-27 00:42:05 +0000

    Using a cursor as a context manager

--- snippets.py
+++ snippets.py
@@ -12,10 +12,14 @@ logging.debug("Database connection established.")
 def put(name, snippet):
     """Store a snippet with an associated name."""
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-    cursor = connection.cursor()
-    command = "insert into snippets values (%s, %s)"
-    cursor.execute(command, (name, snippet))
-    connection.commit()
+    with connection, connection.cursor() as cursor:
+        try:
+            command = "insert into snippets values (%s, %s)"
+            cursor.execute(command, (name, snippet))
+        except psycopg2.IntegrityError as e:
+            connection.rollback()
+            command = "update snippets set message=%s where keyword=%s"
+            cursor.execute(command, (snippet, name))
     logging.debug("Snippet stored successfully.")
     return name, snippet
 
@@ -25,10 +29,14 @@ def get(name):
     Returns the snippet.
     """
     logging.info("Retrieve the snippet with a given name {!r}".format(name))
-    cursor = connection.cursor()
-    command = "select message from snippets where keyword=%s"
-    cursor.execute(command,( name,))
-    message = cursor.fetchone()
+    #cursor = connection.cursor()
+    #command = "select message from snippets where keyword=%s"
+    #cursor.execute(command,( name,))
+    #message = cursor.fetchone()
+    #connection.commit()
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select message from snippets where keyword=%s", (name,))
+        message = cursor.fetchone()
     logging.debug("Snippet retrieved successfully.")
     return message
 
